737f3fa681dfd037f92b71e07736065dc4bfd38b
ChatGTHB <cihat.kose@hotmail.com>
2023-07-04 21:57:14 +0300

    Added Dropdown and Switch test methods.

--- src/test/java/Tests/LoginTest.java
+++ src/test/java/Tests/LoginTest.java
@@ -2,51 +2,96 @@ package Tests;
 
 import io.appium.java_client.AppiumBy;
 import io.appium.java_client.android.AndroidDriver;
-import io.appium.java_client.android.options.UiAutomator2Options;
-import io.appium.java_client.remote.AutomationName;
 import org.openqa.selenium.By;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
-import java.net.MalformedURLException;
-import java.net.URL;
+import utilities.DriverManager;
 import java.time.Duration;
 
-public class LoginTest {
-    static AppiumBy loginMenu =new AppiumBy.ByAccessibilityId("Login");
-    static AppiumBy usernameInputField =new AppiumBy.ByAccessibilityId("input-email");
-    static AppiumBy passwordInputField =new AppiumBy.ByAccessibilityId("input-password");
-    static AppiumBy loginButton =new AppiumBy.ByAccessibilityId("button-LOGIN");
-    static By loginOkButton =new By.ById("android:id/button1");
+public class LoginTest extends DriverManager {
 
-    @Test
+    AndroidDriver driver;
+    WebDriverWait wait;
+
+    @BeforeTest
+    public void setUp(){
+        driver = getAppiumDriver();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+    }
+
+    @Test(priority = 1)
     public static void LoginFunctionTest() throws InterruptedException {
 
-        UiAutomator2Options options = new UiAutomator2Options();
-        options.setPlatformName("Android");
-        options.setPlatformVersion("11.0");
-        options.setAutomationName(AutomationName.ANDROID_UIAUTOMATOR2);
-        options.setDeviceName("Emulator");
-        options.setAppPackage("com.wdiodemoapp");
-        options.setAppActivity("com.wdiodemoapp.MainActivity");
+        WebDriverWait wait = new WebDriverWait(getAppiumDriver(), Duration.ofSeconds(20));
+        Thread.sleep(2000);
 
-        try {
-            AndroidDriver driver = new AndroidDriver(new URL("http://127.0.0.1:4723"), options);
-            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
-            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
-            Thread.sleep(2000);
-            driver.findElement(loginMenu).click();
-            driver.findElement(usernameInputField).sendKeys("username123@gmail.com");
-            driver.findElement(passwordInputField).sendKeys("parola123!");
-            driver.findElement(loginButton).click();
-            wait.until(ExpectedConditions.visibilityOfElementLocated(loginOkButton));
-            driver.findElement(loginOkButton).click();
-            Thread.sleep(3000);
-            driver.quit();
-        } catch (MalformedURLException e) {
-            throw new RuntimeException(e);
+        AppiumBy loginMenu =new AppiumBy.ByAccessibilityId("Login");
+
+        AppiumBy passwordInputField =new AppiumBy.ByAccessibilityId("input-password");
+        getAppiumDriver().findElement(loginMenu).click();
+
+        AppiumBy usernameInputField =new AppiumBy.ByAccessibilityId("input-email");
+        getAppiumDriver().findElement(usernameInputField).sendKeys("username123@gmail.com");
+        getAppiumDriver().findElement(passwordInputField).sendKeys("parola123!");
+
+        AppiumBy loginButton =new AppiumBy.ByAccessibilityId("button-LOGIN");
+        getAppiumDriver().findElement(loginButton).click();
+
+        By successfulLoginMessage=By.id("android:id/message");
+        String actualText=wait.until(ExpectedConditions.visibilityOfElementLocated(successfulLoginMessage)).getText();
+        System.out.println(actualText);
+        Assert.assertEquals(actualText,"You are logged in!");
+
+        By loginOkButton =new By.ById("android:id/button1");
+        wait.until(ExpectedConditions.visibilityOfElementLocated(loginOkButton));
+        getAppiumDriver().findElement(loginOkButton).click();
+
+        Thread.sleep(3000);
+    }
+
+    @Test(priority = 3)
+    public static void Dropdown(){
+        AppiumBy formsButton = (AppiumBy) AppiumBy.accessibilityId("Forms");
+        getAppiumDriver().findElement(formsButton).click();
+        AppiumBy dropdownButton = (AppiumBy) AppiumBy.accessibilityId("Dropdown");
+        getAppiumDriver().findElement(dropdownButton).click();
+        By secondOption = By.xpath("//*[@text='Appium is awesome']");
+        getAppiumDriver().findElement(secondOption).click();
+        By actualSelectedOption = By.xpath("//android.view.ViewGroup[@content-desc=\"Dropdown\"]/android.view.ViewGroup/android.widget.EditText");
+        System.out.println("Selected option is = " + getAppiumDriver().findElement(actualSelectedOption).getText());
+        getAppiumDriver().findElement(dropdownButton).click();
+        String isSelected = getAppiumDriver().findElement(secondOption).getAttribute("checked");
+        Assert.assertEquals(isSelected,"true");
+        System.out.println("Is element selected = " + isSelected);
+    }
+
+    @Test(priority=2)
+    public static void Switch(){
+
+        AppiumBy formsButton = (AppiumBy) AppiumBy.accessibilityId("Forms");
+        getAppiumDriver().findElement(formsButton).click();
+
+        AppiumBy switchText = (AppiumBy) AppiumBy.accessibilityId("switch-text");
+        String switchTextBeforeClick = getAppiumDriver().findElement(switchText).getText();
+        Assert.assertEquals(switchTextBeforeClick, "Click to turn the switch ON");
+        System.out.println("Switch Text Before Click= " + switchTextBeforeClick);
+
+        AppiumBy switchButton = (AppiumBy) AppiumBy.accessibilityId("switch");
+        getAppiumDriver().findElement(switchButton).click();
+
+        String switchTextAfterClick = getAppiumDriver().findElement(switchText).getText();
+        Assert.assertEquals(switchTextAfterClick, "Click to turn the switch OFF");
+        System.out.println("Switch Text After Click = " + switchTextAfterClick);
+    }
+    @AfterTest
+    public void tearDown(){
+        if(driver!=null){
+            getAppiumDriver().quit();
         }
-
     }
 }
 
