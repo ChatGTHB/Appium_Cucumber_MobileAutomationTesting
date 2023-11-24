61d4029eb8feb4faf6bfa38451f5b0694e194889
ChatGTHB <cihat.kose@hotmail.com>
2023-11-24 22:26:36 +0100

    Updated Hooks.java

--- src/test/java/stepDefinitions/Hooks.java
+++ src/test/java/stepDefinitions/Hooks.java
@@ -1,19 +1,17 @@
 package stepDefinitions;
 
-import io.cucumber.java.After;
-import io.cucumber.java.Before;
+import io.cucumber.java.*;
 import utilities.DriverManager;
 
 public class Hooks {
     @Before
-    public void before(){
+    public void before() {
         System.out.println("Scenario started");
     }
 
     @After
-    public void after(){
+    public void after() {
         System.out.println("Scenario finished");
         DriverManager.closeApplication();
     }
-
 }
